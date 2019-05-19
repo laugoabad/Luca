84f1242efe73840982e47131943995192a50a098
laugoabad <36644178+laugoabad@users.noreply.github.com>
2019-05-19 09:44:12 -0300

    l

---
+++ wp-login.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="es-AR">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="es-AR">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Acceder &lsaquo; Luca &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' id='dashicons-css'  href='./wp-includes/css/dashicons.min.css?ver=5.2' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='./wp-includes/css/buttons.min.css?ver=5.2' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='./wp-admin/css/forms.min.css?ver=5.2' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='./wp-admin/css/l10n.min.css?ver=5.2' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='./wp-admin/css/login.min.css?ver=5.2' type='text/css' media='all' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="./wp-content/uploads/2019/05/iconmonstr-microphone-4-240-150x150.png" sizes="32x32" />
+<link rel="icon" href="./wp-content/uploads/2019/05/iconmonstr-microphone-4-240.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="./wp-content/uploads/2019/05/iconmonstr-microphone-4-240.png" />
+<meta name="msapplication-TileImage" content="./wp-content/uploads/2019/05/iconmonstr-microphone-4-240.png" />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-es-ar">
+		<div id="login">
+		<h1><a href="https://es.wordpress.org/">Funciona gracias a WordPress</a></h1>
+	
+	<form name="loginform" id="loginform" action="./" method="post">
+	<p>
+		<label for="user_login">Nombre de usuario o dirección de correo<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Contraseña<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Recuérdame</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Acceder" />
+				<input type="hidden" name="redirect_to" value="./wp-admin/" />
+					<input type="hidden" name="testcookie" value="1" />
+	</p>
+	</form>
+
+			<p id="nav">
+					<a href="./?action=lostpassword">¿Olvidaste tu contraseña?</a>
+				</p>
+	
+	<script type="text/javascript">
+	function wp_attempt_focus(){
+	setTimeout( function(){ try{
+			d = document.getElementById('user_login');
+				d.focus();
+	d.select();
+	} catch(e){}
+	}, 200);
+	}
+
+			wp_attempt_focus();
+			if(typeof wpOnload=='function')wpOnload();
+			</script>
+
+			<p id="backtoblog"><a href="./index.html">
+		&larr; Volver a Luca	</a></p>
+			
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
